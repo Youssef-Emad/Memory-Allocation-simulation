a01b3ff88656052ad87b1c722d90acb69a146ceb
Youssef-Emad <youssef.emad.attia@gmail.com>
2015-05-07 10:04:13 +0200

    add drawig block class

---
+++ memory-allocator/memory-allocator/drawing_block.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace memory_allocator
+{
+    class drawing_block
+    {
+        private string id;
+        private decimal address;
+        private decimal size;
+
+        public drawing_block(string i ,decimal a , decimal s )
+        {
+            id = i;
+            address = a;
+            size = s;
+        }
+
+        public string get_id()
+        {
+            return id;
+        }
+
+        public decimal get_address()
+        {
+            return address;
+        }
+
+        public decimal get_size()
+        {
+            return size;
+        }
+        public void set_id(string num)
+        {
+            id = num;
+        }
+
+        public void set_address(decimal a)
+        {
+            address = a;
+        }
+
+        public void set_size(decimal num)
+        {
+            size = num;
+        }
+    }
+}
