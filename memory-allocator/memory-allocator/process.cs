39fc368da0600f9b67593e870dc7d5b34eb191d4
Youssef-Emad <youssef.emad.attia@gmail.com>
2015-05-07 10:01:57 +0200

    add process class

---
+++ memory-allocator/memory-allocator/process.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace memory_allocator
+{
+    class process
+    {
+        private string id;
+        private decimal size;
+
+        public process(string i, decimal s)
+        {
+            id = i;
+            size = s;
+        }
+        public string get_id()
+        {
+            return id;
+        }
+
+        public decimal get_size()
+        {
+            return size;
+        }
+        public void set_id(string num)
+        {
+            id = num;
+        }
+
+        public void set_size(decimal num)
+        {
+            size = num;
+        }
+    }
+}
