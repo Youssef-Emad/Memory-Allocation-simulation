2e9a6df451884bd68397fdfc61395ee6314dd62d
Youssef-Emad <youssef.emad.attia@gmail.com>
2015-05-07 13:17:47 +0200

    add  (add hole) function

--- memory-allocator/memory-allocator/Form1.cs
+++ memory-allocator/memory-allocator/Form1.cs
@@ -11,6 +11,15 @@ namespace memory_allocator
 {
     public partial class Form1 : Form
     {
+        int holes = 0;
+        int processes = 0;
+
+        List<hole> holes_list = new List<hole>();
+        List<process> processes_list = new List<process>();
+        List<drawing_block> draw_list = new List<drawing_block>();
+
+        decimal total_size = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -33,6 +42,26 @@ namespace memory_allocator
 
         private void hole_button_Click(object sender, EventArgs e)
         {
+            if (holes < holes_count.Value)
+            {
+                if (holes_list.Exists(hole => hole.get_address() == address_input.Value))
+                {
+                    MessageBox.Show("Starting address already exists");
+                }
+                else
+                {
+                    hole h = new hole("Memory", hole_size_input.Value, address_input.Value);
+                    holes_list.Add(h);
+                    total_size += hole_size_input.Value;
+                    holes++;
+                    address_input.Value = 0;
+                    hole_size_input.Value = 0;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Unable to add hole , Please increment the number of holes");
+            }
 
         }
 
