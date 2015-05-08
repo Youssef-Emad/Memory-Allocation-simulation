71abc459b79542e1eb5785b495fc3bf3e0936b03
Youssef-Emad <youssef.emad.attia@gmail.com>
2015-05-08 19:54:18 +0200

    rename form text

--- memory-allocator/memory-allocator/Form1.Designer.cs
+++ memory-allocator/memory-allocator/Form1.Designer.cs
@@ -291,7 +291,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Memory Allocation";
             this.allocation_box.ResumeLayout(false);
             this.allocation_box.PerformLayout();
             this.holes_box.ResumeLayout(false);
